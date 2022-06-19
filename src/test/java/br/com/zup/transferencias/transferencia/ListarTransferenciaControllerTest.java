6ccfc9265541f6e314bc7f8e111161bf0f116b12
Denes <jose.araujo@zup.com.br>
2022-06-19 18:11:22 -0300

    implementacao dos testes de lista vazia do controlador listar transferencias

--- src/test/java/br/com/zup/transferencias/transferencia/ListarTransferenciaControllerTest.java
+++ src/test/java/br/com/zup/transferencias/transferencia/ListarTransferenciaControllerTest.java
@@ -1,8 +1,10 @@
 package br.com.zup.transferencias.transferencia;
 
+import br.com.zup.transferencias.contacorrente.ContaCorrente;
 import br.com.zup.transferencias.contacorrente.ContaCorrenteRepository;
 import br.com.zup.transferencias.exception.ErroPadronizado;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -66,6 +68,37 @@ class ListarTransferenciaControllerTest {
         assertThat(mensagens, containsInAnyOrder("Conta Corrente não encontrada"));
     }
 
-    
+    @Test
+    void deveDevolverUmaListaVaziaCasoContaNaoTenhaTransferencias() throws Exception {
+        // cenário
+        ContaCorrente contaCorrente = new ContaCorrente(
+                "0001",
+                "889638",
+                "joao@zup.com.br",
+                "231.256.710-51",
+                "João"
+        );
 
+        contaCorrenteRepository.save(contaCorrente);
+
+        MockHttpServletRequestBuilder request = get(
+                "/contas/{id}/transferencias", contaCorrente.getId()
+        ).contentType(APPLICATION_JSON);
+
+        // ação e corretude
+        String payloadResponse = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString(UTF_8);
+
+        TypeFactory typeFactory = objectMapper.getTypeFactory();
+
+        List<TransferenciaResponse> transferencias = objectMapper.readValue(payloadResponse,typeFactory.constructCollectionType(
+                List.class,
+                TransferenciaResponse.class
+        ));
+
+        assertTrue(transferencias.isEmpty());
+    }
 }
